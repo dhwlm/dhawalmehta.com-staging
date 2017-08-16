27acc43146a5045962627c82dc4909889302238c
Dhawal M <dhawalmehta91@gmail.com>
2017-08-16 14:12:57 +0530

    update with new theme

---
+++ footer.php
@@ -0,0 +1,6 @@
+<script src="js/imagesloaded.pkgd.min.js"></script>
+<script src="js/anime.min.js"></script>
+<script src="js/charming.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Dhawal Mehta | Developer | Guitarist</title>
+		<link rel="shortcut icon" href="favicon.png">
+		<meta name="description" content="Dhawal Mehta is an experienced User Interface Designer & Developer with a demonstrated history of working in the internet industry.">
+    <meta name="keywords" content="dhawal, Mehta, UI, CMS, Ecommerce, CodeIgniter, Bootstrap, Chrome Extension, Photoshop, Wordpress, AJAX, Responsive Design, Opencart, HTML5, CSS3, jQuery, Javascript, guitar, PRS, Paul Reed Smith, Line6, line 6, POD HD500, POD, HD500, Dhawal Mehta, Dhawal, Mehta">
+		<link href="https://fonts.googleapis.com/css?family=Roboto:400,500|Roboto+Mono" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="css/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="css/demo.css" />
+		<link rel="stylesheet" type="text/css" href="css/pieces.css" />
+		<script>document.documentElement.className = 'js';</script>
+	</head>
+	<body>
